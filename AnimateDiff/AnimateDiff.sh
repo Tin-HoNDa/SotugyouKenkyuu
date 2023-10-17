58b498bb055662a995efa4da476118c60a75233d
Tin-HoNDa <hondasuzuto@gmail.com>
2023-10-17 16:36:50 +0900

    実験開始

--- AnimateDiff/AnimateDiff.sh
+++ AnimateDiff/AnimateDiff.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
 cd AnimateDiff || exit 2
-rm -rf models/StableDiffusion/*
-git clone https://huggingface.co/runwayml/stable-diffusion-v1-5 models/StableDiffusion/
-
-bash download_bashscripts/1-ToonYou.sh
-bash download_bashscripts/2-Lyriel.sh
-bash download_bashscripts/3-RcnzCartoon.sh
-bash download_bashscripts/4-MajicMix.sh
-bash download_bashscripts/5-RealisticVision.sh
-bash download_bashscripts/6-Tusun.sh
-bash download_bashscripts/7-FilmVelvia.sh
-bash download_bashscripts/8-GhibliBackground.sh
 
 pip3 install accelerate triton
+
+torchrun --nnodes=1 --nproc_per_node=1 train.py --config configs/training/training.yaml
+torchrun --nnodes=1 --nproc_per_node=1 train.py --config configs/training/image_finetune.yaml
 
 python -m scripts.animate --config configs/prompts/1-ToonYou.yaml
 python -m scripts.animate --config configs/prompts/2-Lyriel.yaml
@@ -23,6 +15,3 @@ python -m scripts.animate --config configs/prompts/5-RealisticVision.yaml
 python -m scripts.animate --config configs/prompts/6-Tusun.yaml
 python -m scripts.animate --config configs/prompts/7-FilmVelvia.yaml
 python -m scripts.animate --config configs/prompts/8-GhibliBackground.yaml
-
-torchrun --nnodes=1 --nproc_per_node=1 train.py --config configs/training/training.yaml
-torchrun --nnodes=1 --nproc_per_node=1 train.py --config configs/training/image_finetune.yaml
--- AnimateDiff/WebVid.sh
+++ AnimateDiff/WebVid.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/m-bain/webvid.git
 cd webvid || exit 2
-wget http://www.robots.ox.ac.uk/~maxbain/webvid/results_2M_train.csv
+wget http://www.robots.ox.ac.uk/~maxbain/webvid/results_2M_val.csv
 pip3 install pandas numpy requests mpi4py
-python3 download.py --part 0
+python3 download.py --part 0 --csv_path results_2M_val.csv
